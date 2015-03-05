18ed9e8fbb7114bd19a24cfb7acca8825a562c4d
StjerneIdioten <dragonest7@gmail.com>
2015-03-05 20:39:13 +0100

    Adapted for windows

---
+++ Lesson4/Code/SetFuses.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Makefile for programming ATmega32 using assembler
+
+REM For the little blue usbasp programmer
+set PROGRAMMER=-c usbasp -P usb 
+set HighFuse=0xd9
+set LowFuse=0xe4
+
+
+@echo on
+
+avrdude -p m32 %PROGRAMMER% -U hfuse:w:%HighFuse%:m -U lfuse:w:%LowFuse%:m
+
+	
+	
+
+	
+	
---
+++ Lesson4/Lesson4/Lesson4/Lesson4.asm
@@ -0,0 +1,8 @@
+/*
+ * Lesson4.asm
+ *
+ *  Created: 05-03-2015 19:22:51
+ *   Author: StjerneIdioten
+ */ 
+
+
